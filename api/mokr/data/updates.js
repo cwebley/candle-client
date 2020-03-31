bc732450c7b00ff85c2ef580c5b780235ccb2d54
Cameron Webley <cwebley@camerons-mbp.attlocal.net>
2020-03-31 01:34:56 -0500

    batch 100/101 weights

--- api/mokr/data/updates.js
+++ api/mokr/data/updates.js
@@ -3614,7 +3614,7 @@ module.exports = [
   },
   // W9G6 10% Root beer only P-767
   {
-    completedCandleWeightOunces: "16.34",
+    completedCandleWeightOunces: "15.5",
     volumeOverflowPercent: "85",
     colorDescription: "whiteish",
     burns: [],
@@ -3622,7 +3622,7 @@ module.exports = [
   },
   // 3Q6L 10% Vanilla only P-767
   {
-    completedCandleWeightOunces: "16.34",
+    completedCandleWeightOunces: "15.32",
     volumeOverflowPercent: "85",
     colorDescription: "whiteish",
     burns: [],
